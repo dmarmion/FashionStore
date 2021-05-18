2d19ff3f10d357182bd9c52dde7eaa2463c5a6d4
greenmallow <dmarmion08@gmail.com>
2021-05-18 12:50:50 +0000

    Add validation for NewsletterRecipients

--- app/models/newsletter_recipient.rb
+++ app/models/newsletter_recipient.rb
@@ -1,2 +1,7 @@
 class NewsletterRecipient < ApplicationRecord
+  # Email regex from Week 7 Tute
+  validates :email, presence: true#,
+  # I attempted to add email validation here, but couldn't get it working for
+  # some reason
+                    # format: {with: /A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
 end
