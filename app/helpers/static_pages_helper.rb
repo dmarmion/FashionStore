ad62f7cbf83b2c8e51c4fead6d2ef7756db8c233
greenmallow <dmarmion08@gmail.com>
2021-05-21 12:04:28 +0000

    Add top banner featuring random unsaved items

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,4 +1,25 @@
 module StaticPagesHelper
+  # Get a random item that is not in the saved list. Returns nil if there are no
+  # unsaved items remaining.
+  def random_unsaved_item
+    saved_ids = cookies[:saved_list].to_s.split(';')
+    unsaved_ids = []
+    
+    Item.all.each do |item|
+      if !saved_ids.include?(item.id.to_s)
+        unsaved_ids << item.id.to_s
+      end
+    end
+    
+    chosen_id = unsaved_ids.sample
+    
+    if chosen_id
+      return Item.find_by(id: chosen_id)
+    else
+      return nil
+    end
+  end
+  
   # Get a list of the n most popular items
   def most_popular_items(n)
     most_popular = []
